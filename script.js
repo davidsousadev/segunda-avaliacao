ee84f4fb02e00fe44406d69b6d1d8d2068b7815c
davidplay7 <davidk1k3k@gmail.com>
2024-09-18 11:38:19 -0300

    Ajuste para pages

--- script.js
+++ script.js
@@ -146,3 +146,21 @@ modal.addEventListener('click', () => {
 });
 
 
+
+// Carrossel
+
+let index = 0;
+
+function showSlide() {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+
+    index++;
+    if (index >= totalSlides) {
+        index = 0;
+    }
+
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(showSlide, 3000);
